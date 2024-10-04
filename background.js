9a5bb587df593cb9192548d190b7f3f30219a523
Nishanth Vijayan <nishanth@headout.com>
2024-10-05 01:22:02 +0530

    Remove unused files

--- background.js
+++
@@ -1,6 +0,0 @@
-// background.js
-
-chrome.action.onClicked.addListener((tab) => {
-  // When the toolbar icon is clicked, toggle selection mode
-  chrome.tabs.sendMessage(tab.id, {action: 'toggleSelectionMode', enable: true});
-});
